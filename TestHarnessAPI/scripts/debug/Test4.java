5b906f16586f28d5902321b79d25aeb2af53d7b2
Jaco Kok <jaco@dts.za.com>
2011-12-02 10:02:19 +0200

    Pull down

---
+++ TestHarnessAPI/scripts/debug/Test4.java
@@ -0,0 +1,75 @@
+// Test4
+// C:/AXE_Projects/TestHarnessAPI/data/Tests.xml:Tests Test4
+package axe;
+import objectmap.objectmap;
+import resources.axe.*;
+import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.Variant;
+import com.jacob.com.Dispatch;
+import com.rational.test.ft.*;
+import com.rational.test.ft.object.interfaces.*;
+import com.rational.test.ft.object.interfaces.SAP.*;
+import com.rational.test.ft.object.interfaces.WPF.*;
+import com.rational.test.ft.object.interfaces.dojo.*;
+import com.rational.test.ft.object.interfaces.siebel.*;
+import com.rational.test.ft.object.interfaces.flex.*;
+import com.rational.test.ft.object.interfaces.generichtmlsubdomain.*;
+import com.rational.test.ft.script.*;
+import com.rational.test.ft.value.*;
+import com.rational.test.ft.vp.*;
+import com.ibm.rational.test.ft.object.interfaces.sapwebportal.*;
+
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.*;
+import java.io.File;
+import helpers.CustomLib;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+
+public class Test4 extends Test4Helper
+
+{
+
+       public static void testMain(Object[] args)
+
+       {
+
+           objectmap map = new objectmap();         
+
+           axeInit("C:\\AXE_Projects\\TestHarnessAPI\\TestHarnessAPI.tpj", "debug");
+ 
+
+
+			Dispatch.call(axe, "TestBegin", "Test4" ,"Dynamic date", "C:/AXE_Projects/TestHarnessAPI/data/Tests.xml", "Tests", "C:/AXE_Projects/TestHarnessAPI/results/debug/results.xml","");
+			try
+			{
+// 
+// C:/AXE_Projects/TestHarnessAPI/data/Tests.xml:APITest Test02
+				Dispatch.call(axe, "SubtestBegin", "Test02", "Dynamic date", "C:/AXE_Projects/TestHarnessAPI/data/Tests.xml", "APITest");
+				Dispatch.call(axe, "StepBegin", "[]countryInfoByIana", "load.file(countryInfoByIana.xml)", "");  Dispatch.put(axe, "ResultCode", 0);
+				Dispatch.put(axe, "Data",  Dispatch.call(axe, "LoadFile", "countryInfoByIana.xml").toString());
+				axeStepEnd();
+
+				Dispatch.call(axe, "StepBegin", "[]countryInfoByIana", "Set", "");  Dispatch.put(axe, "ResultCode", 0);
+				Dispatch.call(harness, "WsInitialise", Dispatch.call(axe, "GetRunCategoryOption", "webservice", "MobileFish"),  "countryInfoByIana", "http://www.mobilefish.com/services/web_service/countries.php", Dispatch.get(axe, "Data").toString(), Dispatch.call(axe, "GetRunCategoryOptions", "namespace"), Dispatch.call(axe, "GetRunCategoryOptionProperty", "webservice", "MobileFish", "username"), Dispatch.call(axe, "GetRunCategoryOptionProperty", "webservice", "MobileFish", "password"));
+				axeStepEnd();
+
+				Dispatch.call(axe, "StepBegin", "ianacode", "dynamicdate", "");  Dispatch.put(axe, "ResultCode", 0);
+				date = new SimpleDateFormat("dd/MM/yyyy hh:mm a").format(Calendar.getInstance().getTime());
+				axeStepEnd();
+
+				axeSubtestEnd();
+// 
+// 
+            }
+            catch(Exception ex)
+            {
+                axeTestAbort(ex.getMessage());          
+                return;
+            }
+            Dispatch.call(axe, "TestEnd");
+            unregisterAll();
+        }
+}
